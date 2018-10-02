edf0948eefe559612b1cc45d6ce2a1daf43fe1af
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-02 11:00:23 +0100

    function returns an array of years

--- spec/LeapArraySpec.js
+++ spec/LeapArraySpec.js
@@ -11,17 +11,17 @@ describe("LeapArray", function() {
     //     });
     // });
 
-    describe("A function which accepts a startDate and endDate", function() {
-        it("returns the given dates as an Array", function() {
-            expect(leapArray.evaluateYears(2000, 2010)).toEqual([2000, 2010]);
-        });
-    });
-
-    // describe("A function which returns an array of years", function() {
-    //     it("returns all the years between startDate and endDate", function() {
-    //         expect(leapArray.evaluateYears(2000, 2005)).toEqual([2000, 2001, 2002, 2003, 2004]);
+    // describe("A function which accepts a startDate and endDate", function() {
+    //     it("returns the given dates as an Array", function() {
+    //         expect(leapArray.evaluateYears(2000, 2010)).toEqual([2000, 2010]);
     //     });
-    // })
+    // });
+
+    describe("A function which returns an array of years", function() {
+        it("returns all the years between startDate and endDate", function() {
+            expect(leapArray.evaluateYears(2000, 2005)).toEqual([2000, 2001, 2002, 2003, 2004, 2005]);
+        });
+    })
 
 
 
--- src/LeapArray.js
+++ src/LeapArray.js
@@ -3,6 +3,9 @@ function LeapArray() {
 }
 
 LeapArray.prototype.evaluateYears = function(startYear, endYear) {
-    var list = [startYear, endYear];
+    var list = [];
+    for (var i = startYear; i <= endYear; i++) {
+        list.push(i);
+}
     return list;
 }

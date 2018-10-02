a31f1f12910658e967f5e8fb56cd48c63e02282e
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-02 11:01:23 +0100

    remove irrelevant tests

--- spec/LeapArraySpec.js
+++ spec/LeapArraySpec.js
@@ -5,18 +5,6 @@ describe("LeapArray", function() {
         leapArray = new LeapArray();
     });
 
-    // describe("A function which takes a year as a start date", function() {
-    //     it("returns the given start date", function() {
-    //         expect(leapArray.evaluateYears(2000)).toEqual(2000);
-    //     });
-    // });
-
-    // describe("A function which accepts a startDate and endDate", function() {
-    //     it("returns the given dates as an Array", function() {
-    //         expect(leapArray.evaluateYears(2000, 2010)).toEqual([2000, 2010]);
-    //     });
-    // });
-
     describe("A function which returns an array of years", function() {
         it("returns all the years between startDate and endDate", function() {
             expect(leapArray.evaluateYears(2000, 2005)).toEqual([2000, 2001, 2002, 2003, 2004, 2005]);

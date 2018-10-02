8bd085c58c7bb35d7f0f221347debb508341ea28
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-02 10:54:51 +0100

    adds list variable

--- spec/LeapArraySpec.js
+++ spec/LeapArraySpec.js
@@ -17,4 +17,12 @@ describe("LeapArray", function() {
         });
     });
 
+    // describe("A function which returns an array of years", function() {
+    //     it("returns all the years between startDate and endDate", function() {
+    //         expect(leapArray.evaluateYears(2000, 2005)).toEqual([2000, 2001, 2002, 2003, 2004]);
+    //     });
+    // })
+
+
+
 })
--- src/LeapArray.js
+++ src/LeapArray.js
@@ -3,5 +3,6 @@ function LeapArray() {
 }
 
 LeapArray.prototype.evaluateYears = function(startYear, endYear) {
-    return [startYear, endYear];
+    var list = [startYear, endYear];
+    return list;
 }

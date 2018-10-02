a81f40d016a785d7badbd525a3e4a7b07008a2f7
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-02 16:19:37 +0100

    Attempts to extract evaluate function

--- src/LeapArray.js
+++ src/LeapArray.js
@@ -14,3 +14,13 @@ LeapArray.prototype.createArray = function(startYear, endYear) {
 }
     return this.listOfYears;
 }
+
+// LeapArray.prototype.evaluate = function(year) {
+//     if (year % 4 === 0 && year % 100 !== 0) {
+//         this.listOfYears.push(true);
+//     } else if (year % 400 === 0) {
+//         this.listOfYears.push(true);
+//     } else {
+//         this.listOfYears.push(false);
+//     };
+// }

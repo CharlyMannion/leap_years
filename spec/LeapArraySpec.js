f9852d398469992e9a8823fadb67297f6e83c09b
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-03 11:04:37 +0100

    refactors creatArray to be more SOLID

--- spec/LeapArraySpec.js
+++ spec/LeapArraySpec.js
@@ -15,7 +15,7 @@ describe("LeapArray", function() {
    
         it("adds them to the listOfYears", function() {
             leapArray.createArray(2000, 2005);
-            expect(leapArray.listOfYears).toEqual([true, false, false, false, true, false]);
+            expect(leapArray.listOfYears).toEqual([2000, 2001, 2002, 2003, 2004, 2005]);
         })
     });
 
--- src/LeapArray.js
+++ src/LeapArray.js
@@ -4,14 +4,8 @@ function LeapArray() {
 
 LeapArray.prototype.createArray = function(startYear, endYear) {
     for (var year = startYear; year <= endYear; year++) {
-        if (year % 4 === 0 && year % 100 !== 0) {
-            this.listOfYears.push(true);
-        } else if (year % 400 === 0) {
-            this.listOfYears.push(true);
-        } else {
-            this.listOfYears.push(false);
-        };
-}
+        this.listOfYears.push(year);
+    };
     return this.listOfYears;
 }
 

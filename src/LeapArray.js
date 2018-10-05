bdf7b310acf6f9eef063f3c17e9f6b3d0fd57af9
Charlotte Mannion <charlottemannion100@gmail.com>
2018-10-05 10:22:38 +0100

    reverts programme back to two functions

--- src/LeapArray.js
+++ src/LeapArray.js
@@ -12,26 +12,23 @@ LeapArray.prototype.createArray = function(startYear, endYear) {
 LeapArray.prototype.evaluate = function(yearsArray) {
     var assessedList = [];
     yearsArray.forEach(function(yearToAssess) {
-        console.log(LeapArray.isLeap(yearToAssess));
-        var assessedYear = isLeap(yearToAssess);
-        assessedList.push(assessedYear);
-        // if (assessedYear % 4 === 0 && assessedYear % 100 !== 0) {
-        //      assessedList.push(true);
-        //     } else if (assessedYear % 400 === 0) {
-        //     assessedList.push(true);
-        // } else {
-        //     assessedList.push(false);
-        // };
+        if (yearToAssess % 4 === 0 && yearToAssess % 100 !== 0) {
+             assessedList.push(true);
+            } else if (yearToAssess % 400 === 0) {
+            assessedList.push(true);
+        } else {
+            assessedList.push(false);
+        };
     })
     return assessedList;
 }
 
-LeapArray.prototype.isLeap = function(singleYear) {
-    if (singleYear % 4 === 0 && singleYear % 100 !== 0) {
-        return true;
-       } else if (singleYear % 400 === 0) {
-       return true;
-   } else {
-       return false;
-   };
-}
+// LeapArray.prototype.isLeap = function(singleYear) {
+//     if (singleYear % 4 === 0 && singleYear % 100 !== 0) {
+//         return true;
+//        } else if (singleYear % 400 === 0) {
+//        return true;
+//    } else {
+//        return false;
+//    };
+// }
